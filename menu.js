4e09a4144dba32684fc8967ddf8454c0c43922b2
daxovi <48390551+daxovi@users.noreply.github.com>
2022-05-08 22:11:13 +0200

    menu index

---
+++ menu.js
@@ -0,0 +1,9 @@
+const projects = document.getElementsByClassName("projects")[0];
+
+/*
+function toggleMenu() {
+    projects.classList.toggle("active");
+}
+*/
+
+const toggleMenu = () => projects.classList.toggle("active");
